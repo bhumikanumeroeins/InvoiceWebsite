4f1806936c11ddd68da0f7c7c96a46d08e2d7248
Mansi Jain <mansijain018@gmail.com>
2026-01-13 13:53:33 +0530

    api's created, get, get/id, updated , delete api's corrected

--- backend/src/routes/forms/invoiceTaxForm.js
+++ backend/src/routes/forms/invoiceTaxForm.js
@@ -25,9 +25,9 @@ router.post(
 );
 
 /* ---------------- INVOICE CRUD ---------------- */
-router.get("/list", getAllInvoices);
-router.get("/list/:id", getInvoiceById);
-router.put("/update/:invoiceId",upload.fields([
+router.get("/list", authenticateUser, getAllInvoices);
+router.get("/list/:id", authenticateUser, getInvoiceById);
+router.put("/update/:invoiceId",authenticateUser, upload.fields([
     { name: "logo", maxCount: 1 },
     { name: "signature", maxCount: 1 },
     { name: "qrCode", maxCount: 1 }
@@ -35,7 +35,7 @@ router.put("/update/:invoiceId",upload.fields([
   updateInvoice
 );
 
-router.delete("/delete/:id", deleteInvoice);
+router.delete("/delete/:id", authenticateUser, deleteInvoice);
 router.patch(
   "/invoices/:invoiceId/payment-status",
   updatePaymentStatus
