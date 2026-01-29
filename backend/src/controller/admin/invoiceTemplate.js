6b2e39e4b281b956d1b1d2f71c37c03b16880531
Mansi Jain <mansijain018@gmail.com>
2026-01-29 17:22:43 +0530

    added multer in admin invoiceTemplate

--- backend/src/controller/admin/invoiceTemplate.js
+++ backend/src/controller/admin/invoiceTemplate.js
@@ -1,23 +1,42 @@
 import InvoiceTemplate from "../../models/admin/invoiceTemplate.js";
 import bcrypt from "bcryptjs";
 
+// POST create new invoice template
 export const createInvoiceTemplate = async (req, res) => {
   try {
-    const { name, background, layout } = req.body;
+    const { name, layout } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ message: "Template name is required" });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({ message: "Background image is required" });
+    }
+
+    const background = `/uploads/${req.file.filename}`;
 
     const newTemplate = new InvoiceTemplate({
       name,
       background,
-      layout,
-    }); 
+      layout: layout ? JSON.parse(layout) : {},
+    });
+
     await newTemplate.save();
-    res.status(201).json({ message: "Invoice template created", template: newTemplate });
-    } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
-    }
+
+    return res.status(201).json({
+      message: "Invoice template created",
+      template: newTemplate,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
 };
 
-// GET template by name (or id)
+
 export const getInvoiceTemplate = async (req, res) => {
   try {
     const template = await InvoiceTemplate.findOne({
@@ -29,7 +48,7 @@ export const getInvoiceTemplate = async (req, res) => {
       return res.status(404).json({ message: "Template not found" });
     }
 
-    res.json(template);
+    res.status(200).json({ success: true, data: template });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- backend/src/routes/admin/invoiceTemplate.js
+++ backend/src/routes/admin/invoiceTemplate.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 import {createInvoiceTemplate, getInvoiceTemplate} from "../../controller/admin/invoiceTemplate.js";
+import { upload } from "../../config/multer.js";
 // import {authenticateAdmin} from "../../middleware/adminAuth.middleware.js";
 const router = express.Router();
 
-router.post("/create-invoice-template", createInvoiceTemplate);
-router.get("/invoice-template/:id", getInvoiceTemplate);
+router.post("/create-invoice-template",upload.single("background"), createInvoiceTemplate);
+router.get("/:name", getInvoiceTemplate);
 
 
 export default router;
